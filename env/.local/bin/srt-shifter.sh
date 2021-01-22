416102ca07933b2c7eb9836387ba2690a6486aac
Laurent Granié <laurent.granie@gmail.com>
2021-01-22 16:37:34 +0100

    [env] add srt-shifter.sh

--- env/.local/bin/srt-shifter.sh
+++ env/.local/bin/srt-shifter.sh
@@ -5,7 +5,7 @@ set -o errexit -o noclobber -o nounset -o pipefail
 date_offset="$1 seconds"
 
 shift_date() {
-    date --date="$1 $date_offset" +%T,%N | cut -c 1-12
+    date --date="$1Z - 1 hours $date_offset" +%T,%N | cut -c 1-12
 }
 
 while read -r line
@@ -16,7 +16,6 @@ do
         new_start_date="$(shift_date "$start_date")"
         new_end_date="$(shift_date "$end_date")"
         printf "%s %s %s\n" "$new_start_date" "$separator" "$new_end_date"
-        echo "New date"
     else
         printf "%s\n" "$line"
     fi
