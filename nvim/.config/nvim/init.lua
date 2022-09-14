ad17d50a950d8da964da98614194b21f1d6509c4
Laurent Granié <laurent.granie@gfi.fr>
2022-09-14 11:23:01 +0200

    [nvim] add groovy-lsp

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -19,3 +19,11 @@ require("tmux").setup({
   }
 })
 
+require'lspconfig'.groovyls.setup{
+  cmd = {
+    'java',
+    '-jar',
+    '/home/lgranie/opt/groovy-language-server/build/libs/groovy-language-server-all.jar'
+  }  
+}
+
---
+++ nvim/.config/nvim/lua/user/lsp.lua
@@ -0,0 +1,7 @@
+require'lspconfig'.groovyls.setup{
+  cmd = {
+    'java',
+    '-jar',
+    '/home/lgranie/opt/groovy-language-server/build/libs/groovy-language-server-all.jar'
+  }
+}
