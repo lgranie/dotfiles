677a98e8f1ebeab05db86a12ef9f22ed72a07b7c
Laurent Granié <laurent.granie@gfi.fr>
2023-02-24 10:28:13 +0100

    [nvim] add treesitter/rust

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "help", "lua", "yaml" },
+  ensure_installed = { "help", "lua", "yaml", "rust" },
   sync_install = false, 
   auto_install = true,
   highlight = {
